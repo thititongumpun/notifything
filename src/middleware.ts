b33c6182eb32a10a3a95648eb907ea4765a809e9
thititongumpun <thiti180536@gmail.com>
2025-04-03 09:56:00 +0700

    feat: add signup public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)'
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
